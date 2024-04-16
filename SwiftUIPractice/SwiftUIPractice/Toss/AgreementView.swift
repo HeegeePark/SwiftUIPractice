58046f4754932f19e51fc223486cccd75d970da0
kizi <phg980513@naver.com>
2024-04-16 13:36:00 +0900

    [추가] SwiftUIPractice: 동의화면 뷰 레이아웃 어려웠던 부분 해결
    - Spacer(), "maxWidth: .infinity"라는 걸로 해결!
    - HeegeePark/SeSAC#36

--- SwiftUIPractice/SwiftUIPractice/Toss/AgreementView.swift
+++ SwiftUIPractice/SwiftUIPractice/Toss/AgreementView.swift
@@ -9,68 +9,74 @@ import SwiftUI
 
 struct AgreementView: View {
     var body: some View {
-        ZStack(alignment: .leading) {
+        ZStack() {
             Color(.background).ignoresSafeArea()
-            
-            VStack(alignment: .leading, spacing: 200) {
-                VStack(alignment: .leading, spacing: 30) {
-                    Image(.firework)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                    
-                    Text("포인트를 더 모을 수 있게 맞춤 혜택을 추천해드릴까요?")
-                        .font(.title3)
-                        .bold()
-                        .foregroundStyle(.foreground)
-                    
-                    HStack {
-                        Image(.chulcheck)
+            VStack() {
+                HStack {
+                    VStack(alignment: .leading, spacing: 30) {
+                        Image(.firework)
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 30, height: 30)
+                            .frame(width: 50, height: 50)
                         
-                        Text("매일 포인트 받는 출석체크•퀴즈")
-                            .font(.system(size: 16))
-                            .foregroundStyle(.secondary)
-                    }
-                    
-                    HStack {
-                        Image(.event)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30)
+                        Text("포인트를 더 모을 수 있게 맞춤 혜택을 추천해드릴까요?")
+                            .font(.title3)
+                            .bold()
+                            .foregroundStyle(.foreground)
                         
-                        Text("새로운 이벤트")
-                            .font(.system(size: 16))
-                            .foregroundStyle(.secondary)
-                    }
-                    
-                    HStack {
-                        Image(.mission)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30)
+                        HStack {
+                            Image(.chulcheck)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                            
+                            Text("매일 포인트 받는 출석체크•퀴즈")
+                                .font(.system(size: 16))
+                                .foregroundStyle(.secondary)
+                        }
                         
-                        Text("미션 추천")
-                            .font(.system(size: 16))
-                            .foregroundStyle(.secondary)
+                        HStack {
+                            Image(.event)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                            
+                            Text("새로운 이벤트")
+                                .font(.system(size: 16))
+                                .foregroundStyle(.secondary)
+                        }
+                        
+                        HStack {
+                            Image(.mission)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                            
+                            Text("미션 추천")
+                                .font(.system(size: 16))
+                                .foregroundStyle(.secondary)
+                        }
                     }
+                    Spacer()
                 }
+                
+                
+                Spacer()
                 
                 VStack(alignment: .center, spacing: 30) {
                     Button("[선택] 맞춤형 서비스 이용 안내") { }
-                        .font(.caption)
-                        .foregroundStyle(.secondary)
+                        .font(.callout)
+                        .foregroundStyle(.primary)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     
                     Button("동의하기") { }
                         .padding()
                         .font(.system(size: 16))
                         .foregroundStyle(.white)
                         .bold()
+                        .frame(maxWidth: .infinity)
                         .background(.blue)
                         .clipShape(.buttonBorder)
-                        .frame(width: 300, height: 50)
                         
                     
                     Button("다음에 하기") { }
